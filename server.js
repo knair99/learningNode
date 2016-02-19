0454fd59ddb152d80181f2c4d140b66d23c8842b
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-19 00:30:08 -0800

    basic node server - creating an http server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 /**
  * Created by kprasad on 2/18/16.
  */
+
+
+var http = require("http");
+
+http.createServer(function(request, response){
+    console.log("Request received");
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("Hello KK! Keep learning node.js");
+    response.end();
+}).listen(8888);
+
+console.log("Server has started");

1b37ffe60a0eb4ea7017904eb23c8b3d83e923c1
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-22 00:47:52 -0800

    more bs about buffers.toString()

--- buffers_learning.js
+++ buffers_learning.js
@@ -4,10 +4,13 @@
 
 //a buffer is a way to store raw data in node
 
-var buffer = new Buffer(10);
+var buffer = new Buffer("hi there");
 
-for(var i = 0; i < 10; i++){
-    buffer[i] = i;
-}
-
+//for(var i = 0; i < 10; i++){
+//    buffer[i] = i;
+//}
 console.log(buffer);
+
+var op = buffer.toString();
+
+console.log(op);

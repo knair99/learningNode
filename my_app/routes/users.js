c1ceb6cc65b8d88166a00da7490216e22765993f
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-24 01:05:22 -0800

    added new routing with jade templating

--- my_app/routes/users.js
+++ my_app/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../data/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+//Get user list
+router.get('/users', function(req, res, next){
+  res.render('users/index', {title:'Users listing', users: users});
+});
+
+
+//Get each user by name
+router.get('/users/:name', function(req, res, next){
+  var user = users[req.params.name];
+  if(user){
+    res.render('users/profile', {title:'User profile', user: user});
+  }
+  else{
+    next();
+  }
 });
 
 module.exports = router;

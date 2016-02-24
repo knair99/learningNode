eaa5d9fcfead908acd80d2561053073418b25d19
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-24 01:36:53 -0800

    more deugging needed to create new user

--- my_app/routes/users.js
+++ my_app/routes/users.js
@@ -8,18 +8,41 @@ router.get('/', function(req, res, next) {
 });
 
 //Get user list
-router.get('/users', function(req, res, next){
+router.get('/users', function(req, res){
   res.render('users/index', {title:'Users listing', users: users});
 });
 
+//Add new user
+router.get('/users/new', function(req, res, err){
+  res.render('users/new', {title: "New user"});
+});
 
 //Get each user by name
 router.get('/users/:name', function(req, res, next){
   var user = users[req.params.name];
   if(user){
     res.render('users/profile', {title:'User profile', user: user});
+  } else{
+    next();
   }
-  else{
+});
+
+//POST request for users
+router.post('/users', function(req, res){
+  if(users[req.body.username]){
+    res.send('Conflict', 409);
+  } else{
+    users[req.body.username] = req.body;
+    res.redirect('/users');
+  }
+});
+
+//Delete a user
+router.delete('/users/:name', function(req, res, next){
+  if(users[req.params.name]){
+    delete users[req.params.name]; //delete from json object
+    res.redirect('/users');
+  } else {
     next();
   }
 });

82dd785201b1668c0ab85ba43003afdde177ed6f
karunakaran99 <kprasad@Karunakarans-MacBook-Air.local>
2016-02-23 18:17:49 -0800

    right way to pipe output directly from proc

--- pipe_learning.js
+++ pipe_learning.js
@@ -5,6 +5,7 @@
 var spawn = require ('child_process').spawn;
 var http = require('http');
 var fs = require('fs');
+var done = false;
 
 function pipe_process(req, res){
     //piping output of a process
@@ -21,21 +22,26 @@ function pipe_file(req, res){
     rs.pipe(res);
 }
 
-function get_ls(){
-    var child = spawn('ls', ['-latr']);
-    var str = "";
-    child.stdout.pipe(str);
+function get_ls(req, res){
 
-    child.stdout.on('end', function(){
-        child.kill();
-    });
+    for(var i = 0; i < 10; i++) {
 
-    return str;
+        setTimeout(function () {
+            var child = spawn('ls', ['-latr']);
+            child.stdout.pipe(res);
+            child.stdout.on('end', function () {
+                child.kill();
+            });
+        }, 1000);
+    }
+
+    done = true;
 }
 http.createServer(function(req, res){
 
     res.writeHead(200, {'Content-type' : 'text/plain'});
-    res.write(get_ls());
-    res.end();
+    get_ls(req, res);
+
+    //while(!done)
 
 }).listen(8888);
